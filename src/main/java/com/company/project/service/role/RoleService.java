15c22ac7bba4a4c297f86eb81a50a7541ab4cbbb
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-07-12 17:31:14 +0800

    更新代码

---
+++ src/main/java/com/company/project/service/role/RoleService.java
@@ -0,0 +1,7 @@
+package com.company.project.service.role;
+
+/**
+ * @author wangzhj
+ */
+public interface RoleService {
+}
---
+++ src/main/java/com/company/project/service/role/RoleServiceImpl.java
@@ -0,0 +1,8 @@
+package com.company.project.service.role;
+
+import org.springframework.stereotype.Service;
+
+@Service("roleService")
+public class RoleServiceImpl implements RoleService {
+
+}
