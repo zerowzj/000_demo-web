9e4e35912d404bf1db39f440603521d509db4b67
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-07-13 11:29:40 +0800

    更新代码

--- src/main/java/com/company/project/web/controller/role/RoleController.java
+++ src/main/java/com/company/project/web/controller/role/RoleController.java
@@ -2,13 +2,17 @@ package com.company.project.web.controller.role;
 
 import com.company.project.dao.popedomrole.PopedomRoleEO;
 import com.company.project.service.role.RoleService;
+import com.github.miemiedev.mybatis.paginator.domain.PageList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 角色信息控制器
@@ -23,9 +27,15 @@ public class RoleController {
     private RoleService roleService = null;
 
     @RequestMapping("/list")
-    public ModelAndView list(HttpServletRequest request, HttpServletResponse response) {
-
-        return new ModelAndView("/role/roleList");
+    @ResponseBody
+    public Map<String, Object> list(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> model = new HashMap();
+        PageList<PopedomRoleEO> pageLt = roleService.getRolePageLt(null, 1, 10);
+        //
+        model.put("dataLt", pageLt);
+        model.put("page", pageLt.getPaginator());
+        //
+        return model;
     }
 
     @RequestMapping("/toAdd")
