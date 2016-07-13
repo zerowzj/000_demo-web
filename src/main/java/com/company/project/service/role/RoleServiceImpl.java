080a7e7ae11e260c15ac22c775416a8e159981ea
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-07-13 09:39:23 +0800

    更新代码

--- src/main/java/com/company/project/service/role/RoleServiceImpl.java
+++ src/main/java/com/company/project/service/role/RoleServiceImpl.java
@@ -1,8 +1,19 @@
 package com.company.project.service.role;
 
+import com.company.project.dao.popedomrole.PopedomRoleDao;
+import com.company.project.dao.popedomrole.PopedomRoleEO;
+import com.github.miemiedev.mybatis.paginator.domain.PageList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("roleService")
 public class RoleServiceImpl implements RoleService {
 
+    @Autowired
+    private PopedomRoleDao popedomRoleDao = null;
+
+    @Override
+    public PageList<PopedomRoleEO> getRolePageLt(String prName, int pageNo, int pageSize) {
+        return popedomRoleDao.getRolePageLt(prName, pageNo, pageSize);
+    }
 }
--- src/main/java/com/company/project/web/controller/role/RoleController.java
+++ src/main/java/com/company/project/web/controller/role/RoleController.java
@@ -1,8 +1,14 @@
 package com.company.project.web.controller.role;
 
+import com.company.project.dao.popedomrole.PopedomRoleEO;
+import com.company.project.service.role.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 角色信息控制器
@@ -13,9 +19,22 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/role")
 public class RoleController {
 
-    @RequestMapping("/list")
-    public ModelAndView list(){
+    @Autowired
+    private RoleService roleService = null;
 
+    @RequestMapping("/list")
+    public ModelAndView list(HttpServletRequest request, HttpServletResponse response) {
+
+        return new ModelAndView("/role/roleList");
+    }
+
+    @RequestMapping("/toAdd")
+    public ModelAndView toAdd(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("/role/roleAdd");
+    }
+
+    @RequestMapping("/add")
+    public ModelAndView add(HttpServletRequest request, HttpServletResponse response, PopedomRoleEO prEO) {
         return new ModelAndView("/role/roleList");
     }
 }
