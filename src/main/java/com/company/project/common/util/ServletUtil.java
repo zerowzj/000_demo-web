5ad036ccc0f4ba87efc5dc97d336b05408c5cfc5
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-07-13 14:28:11 +0800

    更新代码

---
+++ src/main/java/com/company/project/common/util/ServletUtil.java
@@ -0,0 +1,8 @@
+package com.company.project.common.util;
+
+/**
+ * @author wangzhj
+ */
+public class ServletUtil {
+
+}
---
+++ src/main/java/com/company/project/web/SessionUserInfo.java
@@ -0,0 +1,20 @@
+package com.company.project.web;
+
+/**
+ * Session用户信息
+ *
+ * @author wangzhj
+ */
+public class SessionUserInfo {
+
+    /** 用户编号 */
+    private Long ubId = null;
+
+    public Long getUbId() {
+        return ubId;
+    }
+
+    public void setUbId(Long ubId) {
+        this.ubId = ubId;
+    }
+}
