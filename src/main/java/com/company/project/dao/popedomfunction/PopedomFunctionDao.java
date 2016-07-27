9991f3e1a457930c5c7d329d7c17c78d47cd2e88
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-07-27 13:43:19 +0800

    更新代码

---
+++ src/main/java/com/company/project/dao/popedomfunction/PopedomFunctionDao.java
@@ -0,0 +1,29 @@
+package com.company.project.dao.popedomfunction;
+
+import com.company.project.common.db.BaseDao;
+
+import java.util.List;
+
+/**
+ * 功能Dao
+ *
+ * @author wangzhj
+ */
+public interface PopedomFunctionDao extends BaseDao<PopedomFunctionEO> {
+
+    /**
+     * 获取功能列表
+     *
+     * @param prLevel 角色状态
+     * @return List<PopedomFunctionEO>
+     */
+    public List<PopedomFunctionEO> getFunctionLt(Integer prLevel);
+
+    /**
+     * 获取功能列表
+     *
+     * @param prLevel 角色状态
+     * @return List<PopedomFunctionEO>
+     */
+    public List<PopedomFunctionEO> getRoleFunctionLt(Long prId);
+}
---
+++ src/main/java/com/company/project/dao/popedomfunction/PopedomFunctionDaoImpl.java
@@ -0,0 +1,26 @@
+package com.company.project.dao.popedomfunction;
+
+import com.company.project.common.db.BaseDaoImpl;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository("popedomFunctionDao")
+public class PopedomFunctionDaoImpl extends BaseDaoImpl<PopedomFunctionEO> implements PopedomFunctionDao {
+
+    @Override
+    public List<PopedomFunctionEO> getFunctionLt(Integer prLevel) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("prLevel", prLevel);
+        return getSqlSession().selectList(getSqlId("getFunctionLt"), params);
+    }
+
+    @Override
+    public List<PopedomFunctionEO> getRoleFunctionLt(Long prId) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("prId", prId);
+        return getSqlSession().selectList(getSqlId("getRoleFunctionLt"), params);
+    }
+}
