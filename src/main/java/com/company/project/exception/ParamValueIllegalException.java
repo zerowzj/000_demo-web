e26ebee727aebdae53b40b025f3750a050d3b0e2
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-09-21 10:04:14 +0800

    更新代码

--- src/main/java/com/company/project/exception/ParamValueIllegalException.java
+++ src/main/java/com/company/project/exception/ParamValueIllegalException.java
@@ -8,6 +8,6 @@ package com.company.project.exception;
 public class ParamValueIllegalException extends ParamException {
 
     public ParamValueIllegalException(String paramName, String paramValue){
-        super("1003", "参数值非法[{0}]", paramName, paramValue);
+        super("1003", "参数值非法[{0}, {1}]", paramName, paramValue);
     }
 }
