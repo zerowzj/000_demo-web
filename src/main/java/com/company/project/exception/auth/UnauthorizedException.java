5d5056fe4c815dae57c77f329978079de8b20e14
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-10-27 11:37:58 +0800

    更新代码

--- src/main/java/com/company/project/exception/auth/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.company.project.exception.auth;
-
-/**
- * 未授权异常
- *
- * @author wangzhj
- */
-public class UnauthorizedException extends AuthException {
-}
--- src/main/java/com/company/project/exception/system/SystemException.java
+++
@@ -1,7 +0,0 @@
-package com.company.project.exception.system;
-
-/**
- * Created by wangzhj on 2016/10/13.
- */
-public class SystemException {
-}
