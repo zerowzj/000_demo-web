f6570c189195767d9f4b019505858a2a57f2109a
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-20 11:31:21 +0800

    更新代码

--- src/main/java/com/company/project/web/controller/router/admin/AdminRouter.java
+++ src/main/java/com/company/project/web/controller/router/admin/AdminRouter.java
@@ -5,6 +5,7 @@ import com.company.project.web.controller.action.admin.Action_admin_list;
 import com.company.project.web.pattern.action.ActionExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
